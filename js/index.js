09211cd8ea16d0901e976782d0b6fd64dbb11816
FrankBatista1 <fb53901@gmail.com>
2021-08-23 21:48:44 -0400

    Just the canvas call

--- js/index.js
+++ js/index.js
@@ -3,5 +3,12 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d")
+    const image = {}
+    image.addEventListener('load', e => {
+      ctx.drawImage("/images/road.png", 33, 71, 104, 124, 21, 20, 87, 104);
+    });
+  }
 };
